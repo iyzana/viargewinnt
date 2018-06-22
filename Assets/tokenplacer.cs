782f3dbcdf39870c2282f7abd4e38946fdf0be53
kaiser <jannis.kaiser@adesso.de>
2018-06-22 16:18:13 +0200

    initial commit

---
+++ Assets/tokenplacer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tokenplacer : MonoBehaviour {
+
+    public Transform token;
+
+	// Use this for initialization
+	void Start () {
+        System.Console.Out.WriteLine("abc");
+        Instantiate(token, new Vector3(0f, 0.1f, 0f), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
