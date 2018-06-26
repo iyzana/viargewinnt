d45664a180b42dc24be8b4d820ab0fb23c49b349
jturzinski <janos.turzinski@adesso.de>
2018-06-26 18:43:24 +0200

    menu scene

---
+++ Assets/mainMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour {
+    
+    
+    public Button localGame;
+    public Button startOnline;
+    public Button joinOnline;
+
+	// Use this for initialization
+	void Start () {
+        localGame.GetComponent<Button>().onClick.AddListener(StartLocalGame);
+        startOnline.GetComponent<Button>().onClick.AddListener(StartOnlineGame);
+        joinOnline.GetComponent<Button>().onClick.AddListener(JoinOnlineGame);
+	}
+    
+    void StartLocalGame() {
+         SceneManager.LoadScene("viargewinnt-scene", LoadSceneMode.Additive);
+    }
+    
+    void StartOnlineGame(){
+        
+    }
+    
+    void JoinOnlineGame(){
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
